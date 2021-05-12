7379ee7a782839912b1df4bc50e4a7e1df07bdd7
Lindsey Hight <lindseydhight@gmail.com>
2021-05-12 17:05:01 +0000

    Further study
    Co-authored-by: Arielle Nugent <ariellenugent@gmail.com>

--- server.py
+++ server.py
@@ -38,12 +38,14 @@ def show_madlib():
     color = request.args.get("color")
     noun = request.args.get("noun")
     adjective = request.args.get("adjective")
+    creatures = request.args.get("creatures", "creatures")
 
     return render_template("madlib.html",
                            person=person,
                            color=color,
                            noun=noun,
-                           adjective=adjective)
+                           adjective=adjective,
+                           creatures=creatures)
 
 
 @app.route('/hello')
